37e8e099050d769a5d92dda46bf3699b4276c9e8
Justyna Pietrzak <justyna.p097@gmail.com>
2023-04-18 21:09:21 +0200

    logic for displaying options in select

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-let pokemonList = [
-  "Pikachu",
-  "Bulbasaur",
-  "Charizard",
-  "Onyx",
-  "Caterpie",
-  "Magikarp",
+const pokemonList = [
+  {
+    name: "Pikachu",
+    type: "electric",
+    evolutionLevel: "1",
+    otherForms: "Raichu",
+  },
+  {
+    name: "Bulbasaur",
+    type: "grass/poison",
+    evolutionLevel: "1",
+    otherForms: "Ivysaur, Venusaur",
+  },
+  {
+    name: "Charizard",
+    type: "fire/flying",
+    evolutionLevel: "3",
+    otherForms: "none",
+  },
+  { name: "Onyx", type: "stone", evolutionLevel: "2", otherForms: "none" },
+  {
+    name: "Caterpie",
+    type: "bug/poison",
+    evolutionLevel: "1",
+    otherForms: "none",
+  },
+  {
+    name: "Magikarp",
+    type: "water",
+    evolutionLevel: "1",
+    otherForms: "Gyardos",
+  },
 ];
 
-let Pikachu = {
-  type: "electric",
-  evolutionLevel: "1",
-  otherForms: "Raichu",
-};
+const pokemonsSelect = document.querySelector(".pokemons-select");
+let optionsToSelect = "";
 
-let Bulbasaur = {
-  type: "grass/poison",
-  evolutionLevel: "1",
-  otherForms: "Ivysaur, Venusaur",
-};
-
-let Charizard = {
-  type: "fire/flying",
-  evolutionLevel: "3",
-  otherForms: "none",
-};
-
-let Onyx = {
-  type: "stone",
-  evolutionLevel: "2",
-  otherForms: "none",
-};
-
-let Caterpie = {
-  type: "bug/poison",
-  evolutionLevel: "1",
-  otherForms: "none",
-};
-
-let Magikarp = {
-  type: "water",
-  evolutionLevel: "1",
-  otherForms: "Gyardos",
-};
+pokemonList.forEach((element) => {
+  optionsToSelect = `${optionsToSelect} <option value="${element.name}">${element.name}</option>`;
+});
+console.log(pokemonsSelect);
+pokemonsSelect.innerHTML = optionsToSelect;
