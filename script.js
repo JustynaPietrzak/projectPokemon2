a97ab1dcbd38f4d06c72b3ea7b4dd1a798cc5a8b
Justyna Pietrzak <justyna.p097@gmail.com>
2023-04-18 20:20:43 +0200

    inited project

---
+++ script.js
@@ -0,0 +1,44 @@
+let pokemonList = [
+  "Pikachu",
+  "Bulbasaur",
+  "Charizard",
+  "Onyx",
+  "Caterpie",
+  "Magikarp",
+];
+
+let Pikachu = {
+  type: "electric",
+  evolutionLevel: "1",
+  otherForms: "Raichu",
+};
+
+let Bulbasaur = {
+  type: "grass/poison",
+  evolutionLevel: "1",
+  otherForms: "Ivysaur, Venusaur",
+};
+
+let Charizard = {
+  type: "fire/flying",
+  evolutionLevel: "3",
+  otherForms: "none",
+};
+
+let Onyx = {
+  type: "stone",
+  evolutionLevel: "2",
+  otherForms: "none",
+};
+
+let Caterpie = {
+  type: "bug/poison",
+  evolutionLevel: "1",
+  otherForms: "none",
+};
+
+let Magikarp = {
+  type: "water",
+  evolutionLevel: "1",
+  otherForms: "Gyardos",
+};
