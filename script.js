f80df67d05c67b5f63804b240a4d161a5d3c0715
Justyna Pietrzak <justyna.p097@gmail.com>
2023-05-23 22:04:08 +0200

    added js logic for displaying pages

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 const pokemonList = [
-	{
-		name: "pikachu",
-		type: "electric",
-		"evolution-level": "1",
-		"other-forms": "Raichu",
-		src: "https://nexus.traction.one/images/pokemon/pokemon/25.png",
-	},
-	{
-		name: "bulbasaur",
-		type: "grass/poison",
-		"evolution-level": "1",
-		"other-forms": "Ivysaur, Venusaur",
-		src: "https://nexus.traction.one/images/pokemon/pokemon/1.png",
-	},
-	{
-		name: "charizard",
-		type: "fire/flying",
-		"evolution-level": "3",
-		"other-forms": "none",
-		src: "https://nexus.traction.one/images/pokemon/pokemon/6.png",
-	},
-	{
-		name: "onyx",
-		type: "stone",
-		"evolution-level": "2",
-		"other-forms": "none",
-		src: null,
-	},
-	{
-		name: "caterpie",
-		type: "bug/poison",
-		"evolution-level": "1",
-		"other-forms": "none",
-		src: "https://nexus.traction.one/images/pokemon/pokemon/10.png",
-	},
-	{
-		name: "magikarp",
-		type: "water",
-		"evolution-level": "1",
-		"other-forms": "Gyardos",
-		src: "https://nexus.traction.one/images/pokemon/pokemon/129.png",
-	},
+  {
+    name: "pikachu",
+    type: "electric",
+    "evolution-level": "1",
+    "other-forms": "Raichu",
+    src: "https://nexus.traction.one/images/pokemon/pokemon/25.png",
+  },
+  {
+    name: "bulbasaur",
+    type: "grass/poison",
+    "evolution-level": "1",
+    "other-forms": "Ivysaur, Venusaur",
+    src: "https://nexus.traction.one/images/pokemon/pokemon/1.png",
+  },
+  {
+    name: "charizard",
+    type: "fire/flying",
+    "evolution-level": "3",
+    "other-forms": "none",
+    src: "https://nexus.traction.one/images/pokemon/pokemon/6.png",
+  },
+  {
+    name: "onyx",
+    type: "stone",
+    "evolution-level": "2",
+    "other-forms": "none",
+    src: null,
+  },
+  {
+    name: "caterpie",
+    type: "bug/poison",
+    "evolution-level": "1",
+    "other-forms": "none",
+    src: "https://nexus.traction.one/images/pokemon/pokemon/10.png",
+  },
+  {
+    name: "magikarp",
+    type: "water",
+    "evolution-level": "1",
+    "other-forms": "Gyardos",
+    src: "https://nexus.traction.one/images/pokemon/pokemon/129.png",
+  },
 ];
 
 const pokemonsSelect = document.querySelector(".pokemons-select");
 let optionsToSelect = "";
 
 pokemonList.forEach((element) => {
-	optionsToSelect = `${optionsToSelect} <option class="text-capitalize" value="${element.name}">${element.name}</option>`;
+  optionsToSelect = `${optionsToSelect} <option class="text-capitalize" value="${element.name}">${element.name}</option>`;
 });
 pokemonsSelect.innerHTML = optionsToSelect;
 
@@ -56,15 +56,48 @@ pokemonsSelect.addEventListener("change", setPokemonDataToTemplate);
 setPokemonDataToTemplate();
 
 function setPokemonDataToTemplate() {
-	const selectedPokemon = pokemonList[pokemonsSelect.selectedIndex];
-	for (const property in selectedPokemon) {
-		if (property === "src") {
-			document.querySelector(".portrait").src = selectedPokemon[property];
-		} else {
-			document.querySelector(`.${property}-placeholder`).innerHTML =
-				selectedPokemon[property];
-		}
-	}
+  const selectedPokemon = pokemonList[pokemonsSelect.selectedIndex];
+  for (const property in selectedPokemon) {
+    if (property === "src") {
+      document.querySelector(".portrait").src = selectedPokemon[property];
+    } else {
+      document.querySelector(`.${property}-placeholder`).innerHTML =
+        selectedPokemon[property];
+    }
+  }
+}
+
+// Logic for changing pages
+// These consts should be the same like classes in template 'cause they're related
+
+const HOME_PAGE = "home";
+const YOUR_POKEDEX = "your-pokedex";
+const pages = [HOME_PAGE, YOUR_POKEDEX];
+let currentPage = HOME_PAGE;
+
+const navigationButtons = document.querySelectorAll(".navigation-btn");
+navigationButtons.forEach((btn, index) => (btn.innerHTML = pages[index]));
+
+navigationButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event.target.innerHTML);
+    currentPage = event.target.innerHTML;
+    activeButton(btn);
+    displayCurrentPage();
+  });
+});
+
+function displayCurrentPage() {
+  pages.forEach((page) =>
+    document.querySelector(`.${page}`).classList.add("d-none")
+  );
+  document.querySelector(`.${currentPage}`).classList.remove("d-none");
+  document.querySelector(`.${currentPage}`).classList.add("d-block");
+}
+
+function activeButton(button) {
+  navigationButtons.forEach((btn) => btn.classList.remove("active"));
+  button.classList.add("active");
 }
 
 // const displayPokeLibrary = document.querySelector(".page2");
