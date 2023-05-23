e1f6b34a2e32168631484f20bd5d507387e6aaa4
Wojciech Najda <wojciech.najda.sg@gmail.com>
2023-05-23 20:12:47 +0200

    Wojteks fix

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-import pokemonsData from "./pokemons-data.json" assert { type: "json" };
-
-const pokemonList = pokemonsData;
+const pokemonList = [
+	{
+		name: "pikachu",
+		type: "electric",
+		"evolution-level": "1",
+		"other-forms": "Raichu",
+		src: "https://nexus.traction.one/images/pokemon/pokemon/25.png",
+	},
+	{
+		name: "bulbasaur",
+		type: "grass/poison",
+		"evolution-level": "1",
+		"other-forms": "Ivysaur, Venusaur",
+		src: "https://nexus.traction.one/images/pokemon/pokemon/1.png",
+	},
+	{
+		name: "charizard",
+		type: "fire/flying",
+		"evolution-level": "3",
+		"other-forms": "none",
+		src: "https://nexus.traction.one/images/pokemon/pokemon/6.png",
+	},
+	{
+		name: "onyx",
+		type: "stone",
+		"evolution-level": "2",
+		"other-forms": "none",
+		src: null,
+	},
+	{
+		name: "caterpie",
+		type: "bug/poison",
+		"evolution-level": "1",
+		"other-forms": "none",
+		src: "https://nexus.traction.one/images/pokemon/pokemon/10.png",
+	},
+	{
+		name: "magikarp",
+		type: "water",
+		"evolution-level": "1",
+		"other-forms": "Gyardos",
+		src: "https://nexus.traction.one/images/pokemon/pokemon/129.png",
+	},
+];
 
 const pokemonsSelect = document.querySelector(".pokemons-select");
 let optionsToSelect = "";
 
 pokemonList.forEach((element) => {
-  optionsToSelect = `${optionsToSelect} <option class="text-capitalize" value="${element.name}">${element.name}</option>`;
+	optionsToSelect = `${optionsToSelect} <option class="text-capitalize" value="${element.name}">${element.name}</option>`;
 });
 pokemonsSelect.innerHTML = optionsToSelect;
 
@@ -15,15 +56,15 @@ pokemonsSelect.addEventListener("change", setPokemonDataToTemplate);
 setPokemonDataToTemplate();
 
 function setPokemonDataToTemplate() {
-  const selectedPokemon = pokemonList[pokemonsSelect.selectedIndex];
-  for (const property in selectedPokemon) {
-    if (property === "src") {
-      document.querySelector(".portrait").src = selectedPokemon[property];
-    } else {
-      document.querySelector(`.${property}-placeholder`).innerHTML =
-        selectedPokemon[property];
-    }
-  }
+	const selectedPokemon = pokemonList[pokemonsSelect.selectedIndex];
+	for (const property in selectedPokemon) {
+		if (property === "src") {
+			document.querySelector(".portrait").src = selectedPokemon[property];
+		} else {
+			document.querySelector(`.${property}-placeholder`).innerHTML =
+				selectedPokemon[property];
+		}
+	}
 }
 
 // const displayPokeLibrary = document.querySelector(".page2");
